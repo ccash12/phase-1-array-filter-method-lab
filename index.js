123f402d6a380caf772366ca4023a7f08ab6c6b2
Courtney Cash <cashcourtney97@gmail.com>
2021-08-11 18:27:58 -0400

    commit tree 7a07b046ec533c49206cc0e80e9cb1254933e4cd

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching (driversArray, nameOfDriver) {
+    return driversArray.filter(arrayItem => arrayItem.toLowerCase()===nameOfDriver.toLowerCase()) 
+}
+
+
+function fuzzyMatch(driversArray,relevantLetters) {
+    const driversReturned = driversArray.filter(arrayItem => arrayItem.slice(0,relevantLetters.length)===relevantLetters)
+    return driversReturned
+}
+
+function matchName(driversArray,bobbyName) {
+    const matchingBobbys = driversArray.filter(arrayPairing => arrayPairing.name === bobbyName)
+    return matchingBobbys
+}
